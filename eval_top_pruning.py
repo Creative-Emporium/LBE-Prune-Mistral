6d520da8bccba2806af353e8a4516f252c406532
Falk358 <maxdanelli@gmail.com>
2024-03-06 15:29:57 +0100

    removed not needed memory allocation of model

--- eval_top_pruning.py
+++ eval_top_pruning.py
@@ -22,24 +22,30 @@ def top_layer_pruning(transformer: Transformer, amount: int):
     amount += 1 # increase amount by 1 since we always skip the last layer
     stop_layer :int = num_layers - amount - 1
     layers_to_prune = list(range(num_layers - 1, stop_layer, - 1))
+    prune_count = 0
     for index in layers_to_prune:
         if index == num_layers - 1:
             continue
         print(f"pruned layer with index {index}")
         transformer.layers.pop(str(index))
+        prune_count += 1
     
-    return transformer
+    print(f"prune count: {prune_count}")
+    return transformer, prune_count
 
 
-def eval_top_layer_pruning(model_path:str, transformer: Transformer, tokenizer: Tokenizer, max_tokens = 40, temperature = 0.0, max_amount: int = 14):
+def eval_top_layer_pruning(model_path:str, max_tokens = 40, temperature = 0.0, max_amount: int = 14):
     """ evaluates top layer pruning by pruning layers from top of mistral in steps of 2, starting at at least 2 layers
     """
     mmlu_subset_list = ["high_school_geography"]
     accuracy_amount_pruned = {} # number of pruned layers as key, accuracy as value
+    tokenizer = Tokenizer(str(Path(model_path) / "tokenizer.model"))
     for i in range(0, max_amount+1, 2):
+        transformer, prune_count = Transformer.from_folder(Path(model_path), max_batch_size = 1)
         pruned_transformer = top_layer_pruning(transformer, amount=i)
-        accuracy_amount_pruned[i] = mmlu(model_path=model_path, trans=pruned_transformer, tok=tokenizer, subset_list=mmlu_subset_list, max_tokens=max_tokens, temperature=temperature)
+        accuracy_amount_pruned[prune_count] = mmlu(model_path=model_path, trans=pruned_transformer, tok=tokenizer, subset_list=mmlu_subset_list, max_tokens=max_tokens, temperature=temperature)
         del pruned_transformer
+        del transformer
     
     return accuracy_amount_pruned
 
@@ -50,11 +56,7 @@ def main():
     max_tokens = 40
     temperature = 0.0
     model_path = "model_weights/mistral-7B-v0.2-instruct"
-    tokenizer = Tokenizer(str(Path(model_path) / "tokenizer.model"))
-    transformer = Transformer.from_folder(Path(model_path), max_batch_size = len(prompt))
-    #pruned_transformer = top_layer_pruning(transformer, 3)
-    #acc_after = mmlu(model_path=model_path, trans=pruned_transformer, tok=tokenizer, subset_list=mmlu_subset_list, max_tokens=max_tokens, temperature=temperature)
-    accuracy_amount_pruned = eval_top_layer_pruning(model_path, transformer, tokenizer, max_amount=14)
+    accuracy_amount_pruned = eval_top_layer_pruning(model_path, max_amount=14)
     
     for index, accuracy in accuracy_amount_pruned.items():
         print(f"accuracy when pruning {index} layers is: {accuracy}")
